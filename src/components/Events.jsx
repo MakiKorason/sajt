dae6c589324ac907a5e01d84b11a38ebcf7af146
MakiKorason <danicic.marijana21@gmail.com>
2025-03-04 05:30:10 -1100

    Osvežavanje sajta

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -94,7 +94,7 @@ const Events = () => {
                   Румско културно<br/> лето <BsCalendar2Heart />
                 </Card.Title>
                 <br/><Card.Img style={{width:'240px', borderRadius:'50%'}}  src={kulturnoLeto}/>
-              <p className='container-text'> <br/>Одржавање манифестације je од 28. јуна до 31. августа, у Руми. <BsCalendar2Heart /> </p>
+              <p className='container-text'> <br/>Традиционална манифестација која се сваке године одржава у Руми од 28. јуна до 31. августа. <BsCalendar2Heart /> </p>
                <br/><Button className='btn btn-secondary' onClick={handleShowEvent4}>Детаљније</Button>
               </Card.Body>
             </AnimatedCard >
