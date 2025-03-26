edcba581fdd350136c9f025cea3b25f14e700756
MakiKorason <danicic.marijana21@gmail.com>
2025-03-26 07:47:33 -1100

    Prvi commit

--- src/components/Books/NaDanasnjiDan.jsx
+++ src/components/Books/NaDanasnjiDan.jsx
@@ -1,45 +1,40 @@
-import { Button, Col, Container, Row } from "react-bootstrap"
-import AnimatedCard from "../Department/AnimatedCard"
-import { useState } from "react";
+// import {  Col, Container, Row } from "react-bootstrap"
+// import AnimatedCard from "../Department/AnimatedCard"
 
 
 
-const NaDanasnjiDan =()=>{
+// const NaDanasnjiDan =()=>{
 
 
-
-
-
-    return(<>
+//     return(<>
     
-    <Container>
-      <Row>
-        <Col md={12} className='container-title text-center mt-5'><h1> На данашњи дан (25.3.1947.)  рођен је Елтон Џон</h1><hr/>
-        <p className='container-text'>Реџиналд Двајт био је стидљив дечак са наочарима који је растао у лондонском предграђу Пинер и сањао да постане поп-звезда. Када је напунио двадесет три године, имао је свој први наступ у Америци, у светложутим фармеркама и паром тешких радничких чизама, такође јарко жутих. У том тренутку рођен је Елтон Џон и музички свет се заувек променио.</p>
-        </Col>
-        <Col className='text-center'>
-        <AnimatedCard><iframe width="660" height="415" src="https://www.youtube.com/embed/HLqrimhpjRY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </AnimatedCard>
-    
-    <h1>Поменуту књигу можете пронаћи и у нашој библиотеци.      <a  style={{fontSize:'27px'}} href="https://plus.cobiss.net/cobiss/sr/sr/bib/search?db=gbru" target="_blank" rel="noopener noreferrer">Претражите</a>
-</h1>
-    </Col>
+//     <Container>
+//       <Row>
+//         <Col md={12} className='container-title text-center mt-5'><h1> На данашњи дан (25.3.1947.)  рођен је Елтон Џон</h1><hr/>
+//         <p className='container-text'>Реџиналд Двајт био је стидљив дечак са наочарима који је растао у лондонском предграђу Пинер и сањао да постане поп-звезда. Када је напунио двадесет три године, имао је свој први наступ у Америци, у светложутим фармеркама и паром тешких радничких чизама, такође јарко жутих. У том тренутку рођен је Елтон Џон и музички свет се заувек променио.</p>
+        
+//         <AnimatedCard className='container-iframe'><iframe  src="https://www.youtube.com/embed/HLqrimhpjRY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+//     </AnimatedCard>
+//     <hr/>
+//     <h1>Поменуту књигу можете пронаћи и у нашој библиотеци.      <a  style={{fontSize:'27px'}} href="https://plus.cobiss.net/cobiss/sr/sr/bib/search?db=gbru" target="_blank" rel="noopener noreferrer">Претражите</a>
+// </h1>
+//     </Col>
  
 
  
 
 
 
-    </Row>
+//     </Row>
     
     
     
-    </Container>
+//     </Container>
     
     
     
     
-    </>)
-}
+//     </>)
+// }
 
-export default NaDanasnjiDan
+// export default NaDanasnjiDan
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -154,24 +154,7 @@ const Home =()=> {
     </div></Col>
 </Row>
 
-<Container>
-  <Row>
-    <Col md={12} className='container-title text-center mt-4'><h1> На данашњи дан (25.3.1947.) рођен је Елтон Џон</h1><hr/>
-    <p className='container-text'>Реџиналд Двајт био је стидљив дечак са наочарима који је растао у лондонском предграђу Пинер и сањао да постане поп-звезда. Када је напунио двадесет три године, имао је свој први наступ у Америци, у светложутим фармеркама и паром тешких радничких чизама, такође јарко жутих. У том тренутку рођен је Елтон Џон и музички свет се заувек променио.</p>
-    </Col>
-    <Col className='text-center mb-4'>
-    <AnimatedCard><iframe width="660" height="415" src="https://www.youtube.com/embed/HLqrimhpjRY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</AnimatedCard>
 
-<h1>Поменуту књигу можете пронаћи и у нашој библиотеци. </h1>
-
-<a  style={{fontSize:'27px'}} href="https://plus.cobiss.net/cobiss/sr/sr/bib/search?db=gbru" target="_blank" rel="noopener noreferrer">Претражите</a>
-</Col>
-</Row>
-
-
-
-</Container>
   {/* <Container  className='container-day'>
     <Row className='text-center mt-5'>
       <Col md={12} className=' text-center '>
