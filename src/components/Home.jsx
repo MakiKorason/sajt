20992c7778ea0f8bb7b059dc8d30f0a22e3d7767
MakiKorason <danicic.marijana21@gmail.com>
2025-03-29 02:26:12 -1100

    izmene

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -108,7 +108,7 @@ const Home =()=> {
 />
       <div  className="carousel-caption ">
     
-        <p >Румљанин Атанасије Стојковић је међу првим српским научницима<br/> чији је рад признат на Западу.  
+        <p >Румљанин Атанасије Стојковић је међу првим српским научницима<br/> чији је рад признат на Истоку.  
          <Button
     href="https://www.virtualna-ruma.rs/page.php?pro_id=59" 
       target="_blank"
