d21951cc8e2797c894bd462c037e58e23bc7997b
MakiKorason <danicic.marijana21@gmail.com>
2025-07-04 11:42:23 +0200

    Opis promene

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -203,13 +203,10 @@ const Home =()=> {
 <br/>
 
   </Container>
-
-
-
         <Container className='container-library ms-auto mt-4 mb-4'>
   <Row>
     <Col md={12} className='text-center'>
-      <h2 className='container-title'>Програм културног лета</h2>
+  
       <img className='container-image' src={KulturoLetoProgram} alt="Културно лето 2025"/>
     </Col>
   </Row>
